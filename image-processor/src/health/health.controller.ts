05af9a484b0fe3250b995b6c77c5a09e2a94a77a
sayonara213 <maxsai567@gmail.com>
2026-04-06 08:19:13 +0900

    feat: fix health

--- image-processor/src/health/health.controller.ts
+++ image-processor/src/health/health.controller.ts
@@ -1,5 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService, TypeOrmHealthIndicator } from '@nestjs/terminus';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
 import { RedisHealthIndicator } from './redis.health';
 
 @Controller('health')
